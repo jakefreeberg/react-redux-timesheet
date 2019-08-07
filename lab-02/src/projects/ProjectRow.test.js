fea8d1bbe2c4ce664348168a39d6c8e94b84cf33
Jake Freeberg <jakefreeberg@gmail.com>
2019-08-07 10:55:14 -0500

    Did Lab 02

--- lab-02/src/projects/ProjectRow.test.js
+++ lab-02/src/projects/ProjectRow.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import renderer from 'react-test-renderer';
 import ProjectRow from './ProjectRow';
 
 describe('<ProjectRow />', () => {
-  // TODO Add tests
+  let wrapper;
+  const project = {
+    name: 'NAME',
+    description: 'DESCRIPTION'
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<ProjectRow project={project} />);
+  });
+
+  it('<ProjectRow />', () => {
+    const rowComponent = renderer.create(<ProjectRow project={project} />).toJSON();
+    expect(rowComponent).toMatchSnapshot();
+  });
 });
